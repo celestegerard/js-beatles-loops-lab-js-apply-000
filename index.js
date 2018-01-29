50271ce7b7e81434116312a5ac2c1b29aaf844e4
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-29 22:22:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (i=0; i < musicians.length; i++) {
+    array.push("${musicians}"+" plays "+"${instruments}"+".");
+    i++;
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
